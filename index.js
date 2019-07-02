b0ec374cf4cf461a9011b3afac094108bc791428
Kyle Mumma <kyle.mumma@live.com>
2019-07-02 14:01:20 -0700

    process.env.port port

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ db.serialize(() => {
 })
 
 const app = express();
-app.listen(5000, () => console.log('app is listening on port 5000'));
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`starting server on port ${port}`));
 
 app.use(express.json());
 app.use(cors());
