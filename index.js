85139bdf30cdd23789be5c0c806af2800e8f55c3
Kyle Mumma <kyle.mumma@live.com>
2019-07-02 13:57:54 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+
+// set up database
+let db = new sqlite3.Database('blogpostsdb');
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS posts (id BIGINT, date_posted varchar(55), title varchar(55), day varchar(20), image varchar(512), caption text)");
+})
+
+const app = express();
+app.listen(5000, () => console.log('app is listening on port 5000'));
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/posts', (req, res) => {
+    // select all the ids in the db
+    db.all("SELECT id FROM posts", (err, rows) => {
+        // check if the post is new
+        let isNewPost = true;
+        rows.forEach(row => {
+            if(parseInt(req.body.id) === parseInt(row.id)){
+                isNewPost = false;
+            }
+        });
+
+        if(isNewPost){
+            db.run('INSERT INTO posts VALUES (?, ?, ?, ?, ?, ?)', [req.body.id, req.body.date_posted, req.body.title, req.body.day, req.body.image, req.body.caption], (err) => {
+                if(err) {
+                    console.log('an error has occurred while attempting to insert to the database (index.js: 21)')
+                } else {
+                    console.log(`A post has been inserted with post id ${req.body.id}`)
+                }
+            });
+        } else {
+            console.log('this post is already in the database');
+        }
+    });
+});
+
+app.get('/posts', (req, res) => {
+    db.all("SELECT * FROM posts", (err, rows) => {
+        res.send(rows);
+    });
+});
